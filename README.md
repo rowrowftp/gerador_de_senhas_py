6befd730392fb7dc747f21adcfc3f672461acbe2
Islan Gonzaga <islan.gnz@gmail.com>
2023-09-26 16:37:48 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Gerador de Senha Fortes
+
+Gerador de senhas fortes, gera senhas com caracteres selecionados ou pré selecionados. Digite o tamanho da senha e os caracteres desejados, ou deixe em branco para preenchimento automático.
+
+
+## Funções
+
+- Definir tamanho da senha
+- Definir caracteres presentes
+
+
+## Dependências 
+
+- Python 3.11
+- randint (do módulo random, nativo do Python)
+
+
+## Instalação das dependências (Debian ou Ubuntu)
+
+### Python 3.11
+    sudo apt-get install python3.11 
---
+++ main.py
@@ -0,0 +1,39 @@
+from random import randint
+
+def gera_senha():
+
+    tam = int(input("Digite o tamanho da senha (em números)\n> "))
+    initial = input("\nDigite os caracteres na senha (deixe vazio para preenchimento automático)\n> ")
+    auto = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%¨&*()-_=+[]{}<>,.;:´`\/'"
+
+    chars = "_" * tam
+    charlist = chars.split("_")
+    tamanho = len(initial)-1
+
+    if initial == "":
+        tamanho = len(auto)-1
+        for i in range(tam+1):
+            charlist[i] = auto[randint(0, tamanho)]
+    else:
+        for i in range(tam+1):
+            charlist[i] = initial[randint(0, tamanho)]
+
+    for i in range(tam+1):
+        decisor = randint(0, 1)
+        if decisor == 1:
+            try:
+                charlist[i] = charlist[i].upper()
+            except:
+                pass
+        else:
+            pass
+
+    senha = "".join(charlist)
+    print(f'\nSua senha é: {senha}')
+
+while True:
+    try:
+        gera_senha()
+        break
+    except ValueError:
+        print("\nDigite um tamanho em digitos!\n")
